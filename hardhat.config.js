1b1725ae4b2d0ca5f48f8f230e65cf1b0c0802fa
DMalich <d.malich24@gmail.com>
2023-03-02 12:24:54 +0200

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    defaultNetwork: "hardhat",
+  },
+  namedAccounts: {
+    deployer: {
+        default: 0, 
+        1: 0,
+    },
+}};
