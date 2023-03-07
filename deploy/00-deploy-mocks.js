ac2a5da3c9b642df48d0437c927ccf21884c68c4
DMalich <d.malich24@gmail.com>
2023-03-07 16:16:25 +0200

    successfull deployment on local network

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -7,9 +7,8 @@ const INITIAL_PRICE = "200000000000";
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
 
-    if (developmentChains.includes(chainId)) {
+    if (developmentChains.includes(network.name)) {
         log("Local network detected. Deploying mocks...");
         await deploy("MockV3Aggregator", {
             contract: "MockV3Aggregator",
@@ -17,5 +16,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             log: true,
             args: [DECIMALS, INITIAL_PRICE],
         });
+        log("Mocks deployed!");
+        log("----------------------------------------");
     }
 };
+
+module.exports.tags = ["all", "mocks"];
--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,16 +1,30 @@
 const { network } = require("hardhat");
-const { networkConfig } = require("../helper-hardhat-config");
+const {
+    networkConfig,
+    developmentChains,
+} = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
 
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    let ethUsdPriceFeedAddress;
 
-    const fundMe = await deploy("fundMe", {
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [],
+        args: [ethUsdPriceFeedAddress],
         log: true,
     });
+
+    log("------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
